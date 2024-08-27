2570ea93e1c9feab727f7466a585e014ea805e67
DiMNDev <dimndev@gmail.com>
2024-08-27 06:31:58 -0600

    added beeps and Task.Delay()

--- Program.cs
+++ Program.cs
@@ -1,21 +1,28 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Threading.Tasks;
 
+Timer _timer;
 void countDownFromNumber(int countDownFrom = 10, int currentNumber = -1)
 {
     if (currentNumber < countDownFrom)
     {
         countDownFromNumber(countDownFrom, currentNumber + 1);
         currentNumber++;
-        Console.WriteLine(currentNumber);
+        printCount(currentNumber);
         if (currentNumber == 0)
         {
+            Console.Beep(600, 700);
             Console.WriteLine("BLAST OFF! 🚀");
-            Console.Beep(300, 200);
-            Console.Beep(300, 200);
-            Console.Beep(600, 200);
         }
     }
 
 }
 
-countDownFromNumber(20);
+countDownFromNumber(10);
+
+static async Task printCount(int currentNumber)
+{
+    Task.Delay(1000);
+    Console.Beep(400, 400);
+    Console.WriteLine(currentNumber);
+}

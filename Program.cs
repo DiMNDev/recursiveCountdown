f6fb3b81bb8cff97b680784f5b8fd319a2638031
DiMNDev <Dimndev@gmail.com>
2024-09-07 15:40:32 -0600

    finished recursive countdown

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+void countDownFromNumber(int countDownFrom = 10, int currentNumber = -1)
+{
+    if (currentNumber < countDownFrom)
+    {
+        countDownFromNumber(countDownFrom, currentNumber + 1);
+        currentNumber++;
+        Console.WriteLine(currentNumber);
+        if (currentNumber == 0)
+        {
+            Console.WriteLine("BLAST OFF! 🚀");
+            Console.Beep(300, 200);
+            Console.Beep(300, 200);
+            Console.Beep(600, 200);
+        }
+    }
+
+}
+
+countDownFromNumber(20);
